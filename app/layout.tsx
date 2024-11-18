3ecb4d924ca3bcda070d39b026d4ed0fd62759ba
lea <73567807+lebyleafy@users.noreply.github.com>
2024-11-19 01:23:24 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Quantico, Poppins } from '@next/font/google';
+import {  Poppins } from '@next/font/google';
 import { Exo_2 } from '@next/font/google';
 import Navbar from './navbar';
 import Footer from './footer';
@@ -17,11 +17,7 @@ const exo2 = Exo_2({
   variable: '--font-exo-2',
 });
 
-const quantico = Quantico({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-quantico',
-});
+
 
 export const metadata: Metadata = {
   title: "JBiz - Cryto's Guiding Light",
@@ -40,7 +36,7 @@ export default function RootLayout({
         <link rel="icon" href="/notextlogo.ico" />
       </head>
       <body
-        className={`${quantico.variable} ${exo2.variable} ${poppins.variable} antialiased`}
+        className={` ${exo2.variable} ${poppins.variable} antialiased`}
       >
         <Navbar /> {/* Thêm Navbar */}
         <main>{children}</main> {/* Nội dung chính */}
